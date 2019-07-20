b432bfb94847de537ff80381dea5b9981b04dd0a
Eduard Panasiuk <ed.belarus@gmail.com>
2019-07-20 16:28:08 +0300

    Fix interface

--- Sources/library/Generator.swift
+++ Sources/library/Generator.swift
@@ -8,13 +8,18 @@
 import Foundation
 
 public class Generator {
-    private let content:[String] = [
-        "Hello World",
-        "Bonjour le Monde",
-        "Привет мир"
-    ]
     
-    func generate() -> String? {
+    private let content:[String]
+    
+    public init() {
+        content = [
+            "Hello World",
+            "Bonjour le Monde",
+            "Привет мир"
+        ]
+    }
+    
+    public func generate() -> String? {
         return content.randomElement()
     }
 }
